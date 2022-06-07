9aec38292a9bf6774b5fd18ddaab8a69858b34e5
Chen Zhu <52115330+maxwell2732@users.noreply.github.com>
2022-06-07 20:46:39 +0800

    Add files via upload

---
+++ 220607_RStudio_Extract_rs671.R
@@ -0,0 +1,31 @@
+# 2022.06.07 Extract SNP from Imputation data
+# path on RAP: /Bulk/Genotype Results/Imputation/UKB imputation from genotype/
+# rs671 on Chr 12
+# https://www.snpedia.com/index.php/Rs671
+
+# In Terminal:
+# $ dx download "Bulk/Genotype Results/Imputation/UKB imputation from genotype/ukb22828_c12_b0_v3.bgen"
+# $ dx download "Bulk/Genotype Results/Imputation/UKB imputation from genotype/ukb22828_c12_b0_v3.sample"
+# $ dx download "Bulk/Genotype Results/Imputation/UKB imputation from genotype/ukb22828_c12_b0_v3.bgen.bgi"
+# $ dx download "Bulk/Genotype Results/Imputation/UKB imputation from genotype/ukb22828_c12_b0_v3.mfi.txt"
+
+# In Console:
+library(rbgen)
+
+# Extract rs671
+dd <- bgen.load("ukb22828_c12_b0_v3.bgen", rsid="rs671")
+print( dd$data[ 'rs671',, ] )
+
+# Generate rs671 dataframe
+rs671 <- dd$data[ 'rs671',, ] 
+
+# Check dimension
+dim(rs671)
+
+# Write rs671 as CSV
+write.csv(rs671,'ch12_rs671.csv')
+
+# In Terminal:
+# Upload the CSV file on to Project to avoid auto deletion after the R session
+# $ dx upload ch12_rs671.csv
+
