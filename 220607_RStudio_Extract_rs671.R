15e3d9a1bde06dd5913bbe4c8d8786927311081f
Chen Zhu <52115330+maxwell2732@users.noreply.github.com>
2022-06-08 09:55:48 +0800

    Update 220607_RStudio_Extract_rs671.R

--- 220607_RStudio_Extract_rs671.R
+++ 220607_RStudio_Extract_rs671.R
@@ -10,11 +10,21 @@
 # $ dx download "Bulk/Genotype Results/Imputation/UKB imputation from genotype/ukb22828_c12_b0_v3.mfi.txt"
 
 # In Console:
+# Install rbgen
+# https://enkre.net/cgi-bin/code/bgen/wiki/rbgen
+install.packages( "http://www.well.ox.ac.uk/~gav/resources/rbgen_v1.1.5.tgz", repos = NULL, type = "source" )
+
+# Load rbgen
 library(rbgen)
 
 # Extract rs671
 dd <- bgen.load("ukb22828_c12_b0_v3.bgen", rsid="rs671")
 print( dd$data[ 'rs671',, ] )
+
+# Load directly from RAP Project:
+dd <- bgen.load("/Bulk/Genotype Results/Imputation/UKB imputation from genotype/ukb22828_c7_b0_v3.bgen", rsid="rs2227264")
+dd <- bgen.load("/mnt/project/Bulk/Genotype Results/Imputation/UKB imputation from genotype/ukb22828_c7_b0_v3.bgen", rsid="rs2227264")
+
 
 # Generate rs671 dataframe
 rs671 <- dd$data[ 'rs671',, ] 
